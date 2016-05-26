9d37fa0088a257571cdcf290aae0d272cfbbf5f0
Takeshi Larsson <takeshi.p.larsson@gmail.com>
2016-05-26 14:49:36 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os
+
+from setuptools import setup
+
+repoRootPath = os.path.dirname(os.path.abspath(__file__))
+versionFile = os.path.join(repoRootPath, 'VERSION')
+
+
+def get_version():
+    f = open(versionFile)
+    ver = f.readline().strip('\n')
+    f.close()
+    return ver
+
+
+setup(
+    name="python-gerrit",
+    version=get_version(),
+    author="propyless@github",
+    license='Apache License 2.0',
+    packages=['Gerrit'],
+    include_package_data=True,
+    url='https://github.com/propyless/python-gerrit',
+    description='python-gerrit is a module that interfaces with Gerrits REST API.',
+    install_requires=[
+        "requests"
+    ],
+)
