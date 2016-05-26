19d89764a15a3d8e5d52883a1c3c186fdf758a54
Martin Hagstrom <marhag87@gmail.com>
2016-05-26 16:41:18 +0200

    Add docstrings for init

--- Gerrit/__init__.py
+++ Gerrit/__init__.py
@@ -1,10 +1,25 @@
+"""
+python-gerrit
+=============
+
+A module that uses the Gerrit REST API as an interface to manage
+changes,users, groups, etcetera.
+"""
+
 import json
 
 
 def decode_json(gerrit_response):
+    """
+    Strip Cross Site Script Inclusion prefix from
+    gerrit API response and return it as an object
+    """
+
     return json.loads(gerrit_response.lstrip(')]}\''))
 
 
 def print_message(message, debug=True):
+    """Print debug messages"""
+
     if debug:
         print(message)
