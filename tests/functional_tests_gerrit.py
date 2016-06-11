6cac072bda40ee5de0fa7a75d8fee0db8f28a36c
Martin Hagstrom <marhag87@gmail.com>
2016-06-11 20:38:03 +0200

    Use config file for variables

--- tests/functional_tests_gerrit.py
+++ tests/functional_tests_gerrit.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 import mock
+import yaml
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -19,20 +20,38 @@ class TestGerrit(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         try:
+            with open('test-config.yaml', 'r') as ymlfile:
+                cls.config = yaml.load(ymlfile)
+        except IOError:
+            # No config file, set defaults
+            cls.config = {'webdriver': 'firefox',
+                          'url': 'http://localhost:8080/',
+                          'username': 'felix',
+                          'password': '<password>',
+                         }
+
+        if cls.config.get('webdriver') == 'firefox':
             if os.path.isfile('./wires'):
                 firefox_capabilities = DesiredCapabilities.FIREFOX
                 firefox_capabilities['marionette'] = True
                 firefox_capabilities['binary'] = os.environ.get('firefox_path', '/usr/bin/firefox')
             cls._browser = webdriver.Firefox()
-        except WebDriverException:
+        elif cls.config.get('webdriver') == 'chrome':
             cls._browser = webdriver.Chrome()
+        else:
+            raise Exception('Webdriver not supported')
+
+        cls._url = cls.config.get('url')
+        cls._username = cls.config.get('username')
+        cls._password = cls.config.get('password')
+
         # Log in once to make user admin
-        cls._browser.get('http://localhost:8080/login')
+        cls._browser.get('%slogin' % cls._url)
         cls._browser.implicitly_wait(MAX_WAIT)
         elem = cls._browser.find_element_by_id('f_user')
-        elem.send_keys('felix')
+        elem.send_keys(cls._username)
         elem = cls._browser.find_element_by_id('f_pass')
-        elem.send_keys('<password>' + Keys.RETURN)
+        elem.send_keys(cls._password + Keys.RETURN)
         element = WebDriverWait(cls._browser, MAX_WAIT).until(
             expected_conditions.title_contains('My Reviews')
         )
@@ -44,15 +63,15 @@ class TestGerrit(unittest.TestCase):
     def test_add_project(self):
         # Felix wants to add a project, he uses the gerrit module to do this
         gerrit = Gerrit(
-            url='http://localhost:8080/',
+            url=self._url,
             auth_type='http',
-            auth_id='felix',
-            auth_pw='<password>'
+            auth_id=self._username,
+            auth_pw=self._password,
         )
         project = gerrit.create_project('my project')
 
         # Felix can now access his project in the web interface
-        self._browser.get('http://localhost:8080/#/admin/projects/my+project')
+        self._browser.get('%s#/admin/projects/my+project' % self._url)
         element = WebDriverWait(self._browser, MAX_WAIT).until(
             expected_conditions.title_contains('Project my project')
         )
